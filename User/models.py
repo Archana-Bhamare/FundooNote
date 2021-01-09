37297dccf0aae2d1cda4df256110f7215ec2b7ed
Archana-Bhamare <archusonar2@gmail.com>
2021-01-09 11:12:22 +0530

    Modify : Add user profile model

--- User/models.py
+++ User/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
+
 class UserDetails(models.Model):
-    username = models.CharField(max_length=20)
-    email = models.EmailField(max_length=20)
+    username = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
     password = models.CharField(max_length=20)
     confirm_password = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.username
+
+class Profile(models.Model):
+    user = models.OneToOneField(UserDetails, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.png', upload_to='profile_picture/')
+
