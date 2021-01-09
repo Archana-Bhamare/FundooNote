0dced298b96a5a8a321fba87d46f0f21ea413f4f
Archana-Bhamare <archusonar2@gmail.com>
2021-01-09 11:10:50 +0530

    Modify : register profile model

--- User/admin.py
+++ User/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from User.models import UserDetails
+from User.models import UserDetails, Profile
 
 admin.site.register(UserDetails)
+admin.site.register(Profile)
