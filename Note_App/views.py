bacf3063536c3445bbf9c4675c374de6e1029b87
Archana-Bhamare <archusonar2@gmail.com>
2021-01-11 12:25:45 +0530

    Refactor : provide proper docstring and cleaup the code

--- Note_App/views.py
+++ Note_App/views.py
@@ -4,11 +4,12 @@ from django.utils.decorators import method_decorator
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
+from Note_Project import settings
 from .models import Notes, Label
 from .serializer import CreateNoteSerializer, DisplayNoteSerializer, RestoreNoteSerializer, LabelSerializer
 
 LOG_FORMAT = "%(levelname)s - %(asctime)s - %(message)s"
-logging.basicConfig(filename="C:\\Users\\KatK\\django_projects\\FundooNote_Project\\static\\Notetest.log",
+logging.basicConfig(filename=settings.LOGPATH,
                     level=logging.INFO, format=LOG_FORMAT, filemode='w')
 logger = logging.getLogger()
 
@@ -18,10 +19,9 @@ class createNoteList(GenericAPIView):
     queryset = Notes.objects.all()
     serializer_class = CreateNoteSerializer
 
-    def get(self, request):
+    def get(self):
         """
-
-        @param request: User Id
+        @return: note will be fetch for the user
         """
         queryset = Notes.objects.filter(user_id=self.request.user.id)
         serializer = CreateNoteSerializer(queryset, many=True)
@@ -29,8 +29,8 @@ class createNoteList(GenericAPIView):
 
     def post(self, request):
         """
-
-        Create Note with respective user
+        @param request: requesting for the note data
+        @return: Note successfully created by the user
         """
         serializer = CreateNoteSerializer(data=request.data)
         if serializer.is_valid():
@@ -46,14 +46,13 @@ class diplayNoteList(GenericAPIView):
     serializer_class = DisplayNoteSerializer
     queryset = Notes.objects.all()
 
-    def get(self, request):
+    def get(self):
         """
-
-        it display the notes
+        @return: display the user note
         """
         note = Notes.objects.filter(user_id=self.request.user.id)
-        seri = DisplayNoteSerializer(note, many=True)
-        return Response(seri.data, status=status.HTTP_200_OK)
+        serializer = DisplayNoteSerializer(note, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 method_decorator(login_required(login_url='/login/'), name="dispatch")
@@ -63,22 +62,24 @@ class UpdateNoteList(GenericAPIView):
 
     def get(self, request, pk):
         """
-
-        getting user
+        @param request: user id
+        @param pk: userid of user
+        @return: if note is exist with particular userid then it return 302 status code,
+                if note doesnot exist then it return 404 status code
         """
         try:
             note = Notes.objects.get(id=pk, user_id=self.request.user.id)
             serializer = DisplayNoteSerializer(note)
-            return Response(serializer.data)
-
+            return Response(serializer.data, status=status.HTTP_302_FOUND)
         except Notes.DoesNotExist:
             logger.error("Note Does not Exist")
             return Response("this note does not exist", status=status.HTTP_404_NOT_FOUND)
 
     def put(self, request, pk):
         """
-
-        update the note
+        @param request: user id
+        @param pk: userid of user
+        @return: if note successfully updated then it returns 202 status code
         """
         try:
             note = Notes.objects.get(id=pk, user_id=self.request.user.id)
@@ -95,8 +96,10 @@ class UpdateNoteList(GenericAPIView):
 
     def delete(self, request, pk):
         """
-
-        delete the note
+        @param request: userid
+        @param pk: userid of user
+        @return: If not successfully deleted then it return 202 status code,
+                if that note not found then it returns 404 status code
         """
         try:
             note = Notes.objects.get(id=pk)
@@ -116,12 +119,13 @@ class ArchiveNoteList(GenericAPIView):
 
     def get(self, request):
         """
-
-        it display archive note list
+        @param request: userid
+        @return: if is_archive is True then it return 200 status code and display archive note list
         """
         note = Notes.objects.filter(is_archive=True, user_id=self.request.user.id)
-        seri = DisplayNoteSerializer(note, many=True)
-        return Response(seri.data, status=status.HTTP_200_OK)
+        serializer = DisplayNoteSerializer(note, many=True)
+        logger.info("Note listed Successfully")
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @method_decorator(login_required(login_url='/login/'), name="dispatch")
@@ -130,12 +134,13 @@ class PinNoteList(GenericAPIView):
 
     def get(self, request):
         """
-
-        it display pin note list
+        @param request: userid
+        @return: if is_pin is True then it return 200 status code and display pin note list
         """
         note = Notes.objects.filter(is_pin=True, user_id=self.request.user.id)
-        seri = DisplayNoteSerializer(note, many=True)
-        return Response(seri.data, status=status.HTTP_200_OK)
+        serializer = DisplayNoteSerializer(note, many=True)
+        logger.info("Note listed Successfully")
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @method_decorator(login_required(login_url='/login/'), name="dispatch")
@@ -144,12 +149,13 @@ class TrashNoteList(GenericAPIView):
 
     def get(self, request):
         """
-
-        it display the trash note list
+        @param request: userid
+        @return: if is_trash is True then it return 200 status code and display trash note list
         """
         note = Notes.objects.filter(is_trash=True, user_id=self.request.user.id)
-        seri = DisplayNoteSerializer(note, many=True)
-        return Response(seri.data, status=status.HTTP_200_OK)
+        serializer = DisplayNoteSerializer(note, many=True)
+        logger.info("Note listed Successfully")
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @method_decorator(login_required(login_url='/login/'), name="dispatch")
@@ -161,16 +167,17 @@ class RestoreNoteList(GenericAPIView):
         try:
             note = Notes.objects.get(
                 id=pk, is_trash=True, user_id=self.request.user.id)
-            seri = DisplayNoteSerializer(note)
-            return Response(seri.data, status=status.HTTP_200_OK)
+            serializer = DisplayNoteSerializer(note)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
         except Notes.DoesNotExist:
             return Response("this note does not exit", status=status.HTTP_404_NOT_FOUND)
 
     def put(self, request, pk):
         """
-
-        it restore the note from trash
+        @param request:user id
+        @param pk: userid of user
+        @return: if is_trash is True then it restore note from trash and return 202 status code
         """
         try:
             note = Notes.objects.get(
@@ -178,9 +185,11 @@ class RestoreNoteList(GenericAPIView):
             serializer = RestoreNoteSerializer(note, data=request.data)
             if serializer.is_valid():
                 serializer.save()
+                logger.info("Note successfully restore from the trash")
                 return Response('Restore from trash', status=status.HTTP_202_ACCEPTED)
 
         except Notes.DoesNotExist:
+            logger.error("This note not fouund in trash")
             return Response("this note not found in trash", status=status.HTTP_404_NOT_FOUND)
 
 
@@ -190,34 +199,32 @@ class LabelCreateView(GenericAPIView):
     queryset = Label.objects.all()
 
     def get(self, request):
-
         try:
             user = request.user
             labels = Label.objects.filter(user_id=user.id)
             serializer = LabelSerializer(labels, many=True)
-            logger.info("Got the Labels, from get()")
             return Response(serializer.data, status=200)
         except Exception as e:
-            logger.error("Something went wrong, from get()")
+            logger.error("Something went wrong")
             return Response(e)
 
     def post(self, request):
         """
-
-        create label for note
+        @param request:user request
+        @return: It create label and return 200 status code
         """
         user = request.user
         try:
             label = request.data['label']
             if label == "":
-                logger.error("labelname should be blank, from post()")
+                logger.error("labelname should be blank")
                 return Response({'details': 'labelname should be blank'})
             if Label.objects.filter(user_id=user.id, labelname=label).exists():
-                logger.error("label already exists, from post()")
+                logger.info("Label alreadt exists")
                 return Response({'details': 'label already exists'})
             Label.objects.create(labelname=label, user_id=user.id)
             logger.info("New Label is created.")
-            return Response({'details': 'new label created'})
+            return Response({'details': 'new label created'}, status=status.HTTP_200_OK)
         except Exception as e:
             logger.error("Something went wrong")
             return Response(e)
@@ -234,14 +241,15 @@ class LabelUpdateView(GenericAPIView):
             queryset = Label.objects.filter(user_id=user.id)
             serializer = LabelSerializer(queryset)
             return Response(serializer.data)
-
         except Notes.DoesNotExist:
             logger.error("label Does not Exist")
             return Response("this label does not exist", status=status.HTTP_404_NOT_FOUND)
 
     def put(self, request, id):
         """
-        update label
+        @param request: user request
+        @param id: user id
+        @return: it update the label and return 200 status code
         """
         user = request.user
         try:
@@ -250,20 +258,22 @@ class LabelUpdateView(GenericAPIView):
             serializer = LabelSerializer(user_id, data=data)
             if serializer.is_valid():
                 serializer.save(user_id=user.id)
-                return Response("Success")
+                logger.info("Label successfully updated")
+                return Response("Success",status=status.HTTP_200_OK)
         except:
+            logger.error("Something went wrong")
             return Response("failed")
 
-    def delete(self, request, id):
+    def delete(self,id):
         """
-
-        delete label
+        @param id: user id
+        @return: it successfully delete label and return 200 status code
         """
         try:
             instance = self.request.user.id
             instance.delete()
-            logger.info("Label deleted succesfully")
-            return Response({'details': 'Label deleted succesfully'})
+            logger.info("Label deleted successfully")
+            return Response({'details': 'Label deleted successfully'},status=status.HTTP_200_OK)
         except:
             logger.error("Label not deleted")
             return Response({'details': 'Label not deleted'})
