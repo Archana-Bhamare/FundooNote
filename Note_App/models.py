2403afbde61d6b2eb950c6f887a9b02946198bf0
Archana-Bhamare <archusonar2@gmail.com>
2021-01-16 11:39:41 +0530

    Added : Collaborators to note

--- Note_App/models.py
+++ Note_App/models.py
@@ -1,23 +1,22 @@
-#from datetime import datetime
-
 from django.contrib.auth.models import User
 from django.db import models
-from django.utils import timezone
-# Create your models here.
 from User.models import UserDetails
 
 
 class Label(models.Model):
-    label = models.CharField(max_length=100)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserDetails, on_delete=models.CASCADE)
+    label = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.label
 
 
 class Notes(models.Model):
     user = models.ForeignKey(UserDetails, on_delete=models.CASCADE)
     note_title = models.CharField(max_length=20)
     note_text = models.TextField()
+    label = models.ManyToManyField(to=Label, blank=True)
+    collaborators = models.ManyToManyField(User, blank=True)
     is_archive = models.BooleanField(default=False)
     is_pin = models.BooleanField(default=False)
     is_trash = models.BooleanField(default=False)
-    #date=models.DateTimeField(default=timezone.now, blank=True)
-    label = models.ManyToManyField(Label, blank=True)
