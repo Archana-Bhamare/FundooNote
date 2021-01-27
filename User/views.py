75b264bc2cae456938b1d4ff5d1b093198336b1e
Archana-Bhamare <archusonar2@gmail.com>
2021-01-27 19:31:22 +0530

    Refactor : Refactor the APIs

--- User/views.py
+++ User/views.py
@@ -3,129 +3,138 @@ import jwt
 from django.contrib.auth.models import User
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import EmailMessage
-from django.core.validators import validate_email
-from django.http import HttpResponse
+from django.db.models import Q
+from django.shortcuts import redirect
+from rest_framework.permissions import AllowAny
+from Note_Project import settings
 from django.template.loader import render_to_string
+from drf_yasg.utils import swagger_auto_schema
+from django_short_url.models import ShortURL
 from rest_framework import status, permissions
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-from django_short_url.models import ShortURL as short
 from django_short_url.views import get_surl
-from Note_Project.settings import SECRET_KEY, file_handler
+from Note_Project.settings import file_handler
 from User.models import Profile
 from User.serializer import RegistrationFormSerializer, LoginFormFormSerializer, ForgotPasswordFormSerializer, \
     ResetPasswordFormSerializer, ProfileSerializer
 from User.token import token_activation
-from django.core.exceptions import ValidationError
-from django.db import IntegrityError
-from django.contrib.auth import login, logout
+from django.contrib.auth import authenticate,login, logout
 from rest_framework_jwt.settings import api_settings
 
 jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
 jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logger.addHandler(file_handler)
 
+
 class registerForm(GenericAPIView):
-    """ This is Registration API where user can register"""
     serializer_class = RegistrationFormSerializer
 
+    @swagger_auto_schema(responses={200: RegistrationFormSerializer()})
     def post(self, request):
         """
-        This is the user registration api where user can register by providing JWT token on particular email
-        @param request: User Details
+        This api is for user registration to this application
+        @param request: user registration data like username, email and  password
+        @return: account verification link to registered email once registration is successful
         """
-        try:
-            userName = request.data['username']
-            email = request.data['email']
-            password = request.data['password']
-            confirm_password = request.data['confirm_password']
-
-            if userName == "" or email == "" or password == "":
-                logger.error("You can not put empty fields")
-                return Response("You can not put empty fields", status=status.HTTP_406_NOT_ACCEPTABLE)
-            if password == confirm_password:
-                try:
-                    validate_email(email)
-                    user = User.objects.create(username=userName, email=email, password=password)
-                    user.is_active = False
-                    user.save()
-                    current_site = get_current_site(request).domain
-                    payload = jwt_payload_handler(user)
-                    token = jwt_encode_handler(payload)
-                    url = str(token)
-                    surl = get_surl(url)
-                    short_token = surl.split('/')
-
-                    mail_subject = "Click link for activating "
-                    msg = render_to_string('email_validation.html', {
-                        'user': userName,
-                        'domain': current_site,
-                        'surl': short_token[2]
-                    })
-                    recipients = email
-                    email = EmailMessage(mail_subject, msg, to=[recipients])
-                    email.send()
-                    logger.info("Confirmation email sent")
-                    return Response('Please confirm your email address to complete the registration',
-                                    status=status.HTTP_200_OK)
-                except ValidationError:
-                    logger.error("Email Not Found")
-                    return Response("Email not found", status=status.HTTP_404_NOT_FOUND)
-            else:
-                logger.error("Password Mismatch")
-                return Response("Password Mismatch", status=status.HTTP_400_BAD_REQUEST)
-        except IntegrityError:
-            logger.error("User Already Exist")
-            return Response("User Already exist")
-
+        data = request.data
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
+        confirm_password = data.get('confirm_password')
+        if password != confirm_password:
+            logger.error("password mismatch")
+            return Response("password mismatch ", status=status.HTTP_400_BAD_REQUEST)
+        user_name = User.objects.filter(
+            Q(username__iexact=username)
+        )
+        user_email = User.objects.filter(
+            Q(email__iexact=email)
+        )
+        if user_name.exists():
+            logger.error("Username already exist")
+            return Response("Username already exist", status=status.HTTP_400_BAD_REQUEST)
+        elif user_email.exists():
+            logger.error("Email id already exist")
+            return Response("Email id already exist", status=status.HTTP_400_BAD_REQUEST)
+        else:
+            user = User.objects.create(username=username, email=email)
+            user.set_password(password)
+            user.is_active = False
+            user.save()
+            payload = jwt_payload_handler(user)
+            token = jwt_encode_handler(payload)
+            url = str(token)
+            surl = get_surl(url)
+            short_token = surl.split("/")
+            msg = "Activate your account by clicking below link"
+            mail_message = render_to_string('email_validation.html', {
+                'user': user.username,
+                'domain': get_current_site(request).domain,
+                'surl': short_token[2]
+            })
+            recipient_email = user.email
+            recipients = email
+            email = EmailMessage(msg, mail_message, to=[recipients])
+            email.send()
+            logger.info("Confirmation email sent")
+            return Response('Please confirm your email address to complete the registration',
+                            status=status.HTTP_200_OK)
 
 def activate(request, surl):
     """
-    Here user activate his email account by clicking on provided link
-    @param request: activate account
-    @param surl: Short url-Token
-    @return: successfully activate email account
+        @param request: once the account verification link is clicked by user this will take that request
+        @return: it will redirect to login page
     """
     try:
-        token_object = short.objects.get(surl=surl)
-        token = token_object.lurl
-        decode = jwt.decode(token, SECRET_KEY)
-        user_name = str(decode['username'])
-        user = User.objects.get(username=user_name)
+        tokenobject = ShortURL.objects.get(surl=surl)
+        token = tokenobject.lurl
+        decode = jwt.decode(token, settings.SECRET_KEY)
+        username = decode['username']
+        user = User.objects.get(username=username)
         if user is not None:
             user.is_active = True
             user.save()
             logger.info("successfully activate your account")
-            return HttpResponse("successfully activate your account......")
+            return redirect('login')
         else:
-            logger.info("Something went wrong")
-            return Response("Something went wrong")
+            logger.error("User not valid")
+            return Response("User not valid")
     except KeyError:
         logger.error("Key Error")
         return Response("Key Error")
 
 
 class loginForm(GenericAPIView):
-    """ This Login API used to user Login"""
     serializer_class = LoginFormFormSerializer
+    permission_classes = (AllowAny,)
 
     def post(self, request):
         """
-        @param request: Username, Password
-        User Login
+        This API is used to authenticate user to access resources
+        @param request: user credential like username and password
+        @return: Redirects to my basic home page
         """
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            user_data = serializer.data
-            user = User.objects.get(username=user_data['username'],password=user_data['password'])
-            login(request, user)
-            return Response(f"{user_data} : Login successfully", status=status.HTTP_202_ACCEPTED)
-        else:
-            return Response("Log in Failed", status=status.HTTP_406_NOT_ACCEPTABLE)
+        data = request.data
+        username = data.get('username')
+        password = data.get('password')
+        user = authenticate(username=username, password=password)
+        try:
+            check = User.objects.filter(
+                Q(username__iexact=username) or
+                Q(password__iexact=password)
+            )
+            if check.count() == 1:
+                login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+                logger.info("Login successful")
+                return Response(f"{user} : User Login successfully...", status=status.HTTP_200_OK)
+        except:
+            logger.error("User not exist")
+            return Response("User not exist", status=status.HTTP_403_FORBIDDEN)
+
 
 class logoutForm(GenericAPIView):
     """ This Logout API used to logout the user"""
@@ -213,29 +222,26 @@ class resetPasswordForm(GenericAPIView):
             return Response("password mismatch")
 
 
-class UserProfileForm(GenericAPIView):
-    """ This API used to update the user profile"""
+class ProfileUpdate(GenericAPIView):
     serializer_class = ProfileSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request):
         """
-        This function update the user profile
-        @param request: profile files
-        @return: profile updated
+            This API is used to update user profile
+            @param: user profile data
+            @return: updates user profile
         """
         img = request.FILES['image']
-        print(img)
         try:
             user = Profile.objects.get(user=request.user)
             serializer = self.serializer_class(user, data={'image': img})
+
             if serializer.is_valid():
                 serializer.save()
-                logger.info("Profile Successfully Updated")
-                return Response('Profile Successfully Updated', status=status.HTTP_200_OK)
+                logger.info('Profile image updated')
+                return Response('Profile image updated', status=status.HTTP_200_OK)
             else:
-                logger.error("Profile Update Failed")
-                return Response("Profile Update Failed", status=status.HTTP_400_BAD_REQUEST)
+                return Response(serializer.errors, status=400)
         except:
-            logger.error("Something went wrong")
-            return Response("Something went wrong", status=status.HTTP_400_BAD_REQUEST)
+            return Response("Error", status=400)
