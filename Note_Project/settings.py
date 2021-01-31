1b2819fb987fa5114130aa072f7c54fc89e21186
Archana-Bhamare <archusonar2@gmail.com>
2021-01-31 21:16:27 +0530

    Refactor : refactor database

--- Note_Project/settings.py
+++ Note_Project/settings.py
@@ -87,10 +87,10 @@ WSGI_APPLICATION = 'Note_Project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('NAME'),
-        'USER': config('USER'),
-        'PASSWORD': config('PASSWORD'),
-        'HOST': config('HOST'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
         'PORT': 5432,
     }
 }
